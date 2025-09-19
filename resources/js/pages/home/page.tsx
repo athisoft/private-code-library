acd8388c29381d45841a66eaf685bc2c310a2ab4
AthiSoft <teamvenu058@gmail.com>
2025-09-19 09:44:34 +0530

    type effect

--- resources/js/pages/home/page.tsx
+++ resources/js/pages/home/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Head } from '@inertiajs/react'
+import { Head, Link } from '@inertiajs/react'
 import HomeNavigation from '../common/home-navigation'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import homecss from './home.module.css'
 
 
 export default function Home() {
@@ -13,10 +14,11 @@ export default function Home() {
 
             <HomeNavigation />
 
-            <main className=' w-full min-h-dvh'>
+            <main className="min-h-full w-full">
 
-                <div className='flex flex-wrap sm:px-5 items-center'>
-                    <div className='md:w-[50%] w-full'>
+                <section className='sm:px-5 items-center  grid grid-cols-1 md:grid-cols-2'>
+
+                    <div className='w-full'>
                         <DotLottieReact
                             // className='max-h-[100px]'
                             height={250}
@@ -26,22 +28,23 @@ export default function Home() {
                         />
                     </div>
 
-                    <div className='md:w-[50%] w-full'>
+                    <div className='w-full'>
                         <div className='sm:px-0 px-4'>
-                            <h1 className='font-semibold lg:text-7xl text-5xl mb-2'>Code Library</h1>
+                            <h1 className={'font-semibold lg:text-7xl text-5xl inline-flex items-baseline gap-2'}>
+                                Code <span className={homecss.typewriter}> Library</span></h1>
                             <p className='font-thin text-lg '>Keep your code organized, searchable, and accessible whether it's a full project or a quick snippet. Platforms like GitHub, CodeKeep, and CodePen help you save time, stay consistent, and revisit your work whenever inspiration strikes.</p>
                         </div>
 
                     </div>
 
-                    <div className='text-center self-end'>
-                        <p>All rights reserved - {currentYear}</p>
-                    </div>
-                </div>
-
-
+                </section>
 
             </main>
+
+            <footer className='text-center py-2 justify-self-end bg-green-950'>
+                <p>© {currentYear}
+                    <a className='font-semibold' href={`https://www.athisoft.in`} target='_blank' rel='noopener noreferrer'> Athisoft</a>. All rights reserved. Crafted with care and code.</p>
+            </footer>
 
         </>
     )
