744383b42c02750389ee8a835033bdd557b6093a
athisoft <teamvenu058@gmail.com>
2025-09-26 18:52:25 +0530

    more will be added soon

---
+++ resources/js/pages/dashboard/components/card.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className="relative w-full sm:w-fit card m-2">
+
+            <div className="sm:min-w-[250px] sm:min-h-[250px] min-h-[200px] w-full bg-amber-400 rounded-sm relative">
+                <img className='w-fit h-[250px]' src="/images/Placeholder.svg" alt="" />
+
+                <div className='absolute' style={{backgroundColor:"red"}}>
+                  <p className=''>Code Description</p>
+                </div>
+            </div>
+
+
+            <button
+                type="button"
+                className="absolute bottom-2 right-2 py-2 px-3 bg-blue-600 text-white font-bold rounded-sm cursor-pointer hover:bg-blue-700">
+                Copy Code
+            </button>
+        </div>
+    )
+}
+
+export default Card
--- resources/js/pages/dashboard/page.tsx
+++ resources/js/pages/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { DropdownMenu, DropdownMenuItem } from '@/components/ui/dropdown-menu'
 import { Head } from '@inertiajs/react'
+import { Search } from 'lucide-react'
+import Card from './components/card'
 
 
 function Dashboard() {
@@ -25,38 +27,62 @@ function Dashboard() {
 
     return (
         <section className=''>
-            <Head title='Codebase Libraries'/>
+            <Head title='Codebase Libraries' />
 
             {/* Filters */}
-            <div className='flex'>
+            <div className='flex justify-around flex-wrap gap-2'>
+
                 <div className='flex'>
-                    <input type="text" placeholder='Find your code snippets'/>
-                    <button>Search</button>
+                    <div className="flex items-center border border-gray-300 rounded-lg px-2 py-2 shadow-sm focus-within:ring-2 focus-within:ring-blue-500">
+                        <Search className="h-5 w-5 text-gray-500" />
+                        <input
+                            type="text"
+                            placeholder="Find your code snippets"
+                            className="w-full ml-2 outline-none bg-transparent text-gray-700 placeholder-gray-400"
+                        />
+                    </div>
+
+                    <select className='px-2 py-2 rounded-sm' name="language" id="language">
+                        <option className='bg-green-600' value="">Select Language</option>
+                        {Object.entries(languages).map(([key, value]) => (
+                            <option key={key} className="bg-green-600" value={key}>
+                                {key}
+                            </option>
+                        ))}
+                    </select>
+
+                    <select className='px-2 py-2 rounded-sm' name="date" id="date">
+                        <option className='bg-green-600' value="">Filter by Date</option>
+                        {Object.entries(date).map(([key, value]) => (
+                            <option key={key} className="bg-green-600" value={key}>
+                                {key}
+                            </option>
+                        ))}
+                    </select>
                 </div>
 
-                <select className='px-2 py-2 rounded-sm' name="language" id="language">
-                    <option className='bg-green-600' value="">Select Language</option>
-                    {Object.entries(languages).map(([key, value]) => (
-                        <option key={key} className="bg-green-600" value={key}>
-                            {key}
-                        </option>
-                    ))}
-                </select>
 
-                <select className='px-2 py-2 rounded-sm' name="date" id="date">
-                    <option className='bg-green-600' value="">Filter by Date</option>
-                    {Object.entries(date).map(([key, value]) => (
-                        <option key={key} className="bg-green-600" value={key}>
-                            {key}
-                        </option>
-                    ))}
-                </select>
+                <div className=''>
+                    <button type="button" className='px-2 py-2 bg-green-600 hover:bg-green-700 cursor-pointer rounded-sm'>Add Code</button>
+                </div>
 
             </div>
 
             {/* Code Thumbnails + Preview */}
-            <div>
+            <div className='flex flex-wrap p-5 justify-center'>
 
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+
+                <Card />
+
+            </div>
+            
+            {/* Pagination */}
+            <div>
+                    
             </div>
 
 
